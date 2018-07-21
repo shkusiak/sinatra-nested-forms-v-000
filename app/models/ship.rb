99f2fb496d81e7123cdd7107a1f87a6ff6d0fb36
Shea Kusiak <shkusiak@gmail.com>
2018-07-21 19:14:06 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,21 +2,21 @@ class Ship
 
   attr_accessor :name, :type, :booty
 
-  SHIPS = []
+  @@all = []
 
   def initialize(params)
     @name = params[:name]
     @type = params[:type]
     @booty = params[:booty]
-    SHIPS << self
+    @@all << self
   end
 
   def self.all
-    SHIPS
+    @@all
   end
 
   def self.clear
-    SHIPS = []
+    @@all = []
   end
 
 end
