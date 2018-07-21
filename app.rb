eb2ac4b324476df9befcee91c4cf748aba857dae
Shea Kusiak <shkusiak@gmail.com>
2018-07-21 19:12:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      
+    end
+
+    get '/new' do
+      
+    end
+
+    post '/pirates' do
+      
+    end
 
   end
 end
